13c4e29742410e77e54067dc0dfe16bdb8783967
MJCappella <mjaymickey001@gmail.com>
2025-03-02 14:30:24 +0300

    add email configuration and setup for Flask-Mail

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_mail import Mail, Message
 
 from flask_login import (
     UserMixin,
@@ -21,6 +22,14 @@ db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Use your mail server
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'your-email@gmail.com'  # Replace with your email
+app.config['MAIL_PASSWORD'] = 'your-email-password'  # Replace with your email password
+app.config['MAIL_DEFAULT_SENDER'] = 'your-email@gmail.com'  # Replace with your sender email
+
+mail = Mail(app)
 
 def create_app():
     app = Flask(__name__)
