9a0134940f82f24e83c2189404bae8544a542c9c
MJCappella <mjaymickey001@gmail.com>
2025-03-02 15:11:50 +0300

    refactor email configuration setup by moving settings to a separate config module

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
-from flask_mail import Mail, Message
+from config import mail_configs
 
 from flask_login import (
     UserMixin,
@@ -22,15 +22,6 @@ db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Use your mail server
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your-email@gmail.com'  # Replace with your email
-app.config['MAIL_PASSWORD'] = 'your-email-password'  # Replace with your email password
-app.config['MAIL_DEFAULT_SENDER'] = 'your-email@gmail.com'  # Replace with your sender email
-
-mail = Mail(app)
-
 def create_app():
     app = Flask(__name__)
 
@@ -42,5 +33,6 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
+    mail_configs()
     
     return app
