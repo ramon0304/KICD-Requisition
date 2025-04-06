aaf95c609dc7e8dd3f0593f92d02498e0ff6001f
MJCappella <mjaymickey001@gmail.com>
2025-04-06 10:49:52 +0300

    created a new column to track admin users

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     pwd = db.Column(db.String(300), nullable=False, unique=True)
+    is_admin = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
